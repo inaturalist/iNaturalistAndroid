d47a37925e70bc438eac54bb016ded3e56a917f0
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2011-12-26 11:57:27 -0500

    Added grey notification icon.

--- gen/org/inaturalist/android/R.java
+++ gen/org/inaturalist/android/R.java
@@ -14,14 +14,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_maps_indicator_current_position=0x7f020001;
         public static final int ic_right_angle_quotation_mark_ornament=0x7f020002;
-        public static final int logo_text=0x7f020003;
-        public static final int mm_34_dark_magenta=0x7f020004;
-        public static final int mm_34_dodger_blue=0x7f020005;
-        public static final int mm_34_golden_rod=0x7f020006;
-        public static final int mm_34_hot_pink=0x7f020007;
-        public static final int mm_34_inat_green=0x7f020008;
-        public static final int mm_34_orange_red=0x7f020009;
-        public static final int mm_34_unknown=0x7f02000a;
+        public static final int ic_stat_inaturalist=0x7f020003;
+        public static final int logo_text=0x7f020004;
+        public static final int mm_34_dark_magenta=0x7f020005;
+        public static final int mm_34_dodger_blue=0x7f020006;
+        public static final int mm_34_golden_rod=0x7f020007;
+        public static final int mm_34_hot_pink=0x7f020008;
+        public static final int mm_34_inat_green=0x7f020009;
+        public static final int mm_34_orange_red=0x7f02000a;
+        public static final int mm_34_unknown=0x7f02000b;
     }
     public static final class id {
         public static final int accuracy=0x7f060017;
