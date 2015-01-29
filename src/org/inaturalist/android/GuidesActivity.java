85dd07c6acb57a52a1c0b1fec9c0fce213ce47c0
budowski <budowski@gmail.com>
2015-01-28 23:31:00 -0800

    Side menu support for guides/projects screens (overlapping the tab headers)

--- src/org/inaturalist/android/GuidesActivity.java
+++ src/org/inaturalist/android/GuidesActivity.java
@@ -1,30 +1,21 @@
 package org.inaturalist.android;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.ActionBar.Tab;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.actionbarsherlock.view.MenuItem;
 import com.flurry.android.FlurryAgent;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTabHost;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
-import android.content.Context;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 
-public class GuidesActivity extends BaseFragmentActivity {
+public class GuidesActivity extends BaseFragmentActivity implements OnTabChangeListener, OnPageChangeListener {
+    MyPageAdapter mPageAdapter;
     private ViewPager mViewPager;
-    private TabsAdapter mTabsAdapter;
+    private TabHost mTabHost;   
     
 	@Override
 	protected void onStart()
@@ -46,28 +37,71 @@ public class GuidesActivity extends BaseFragmentActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.guides);
-        onDrawerCreate(savedInstanceState);
+	    onDrawerCreate(savedInstanceState);
         
-        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
         
-        setupUI();
-    }
-    
-   
-    private void setupUI() {
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        // Tab Initialization
+        initialiseTabHost();
 
-        // Add the tabs
-        mTabsAdapter = new TabsAdapter(this, actionBar, mViewPager);
-        mTabsAdapter.addTab(actionBar.newTab().setText(R.string.all_guides),
-                AllGuidesTab.class, null);
-        mTabsAdapter.addTab(actionBar.newTab().setText(R.string.my_guides),
-                MyGuidesTab.class, null); 
-        mTabsAdapter.addTab(actionBar.newTab().setText(R.string.nearby_guides),
-                NearByGuidesTab.class, null); 
+        // Fragments and ViewPager Initialization
+        List<Fragment> fragments = getFragments();
+        mPageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
+        mViewPager.setAdapter(mPageAdapter);
+        mViewPager.setOnPageChangeListener(this);
     }
     
+     
+    // Method to add a TabHost
+    private static void AddTab(GuidesActivity activity, TabHost tabHost, TabHost.TabSpec tabSpec) {
+        tabSpec.setContent(new MyTabFactory(activity));
+        tabHost.addTab(tabSpec);
+    }
+
+    // Manages the Tab changes, synchronizing it with Pages
+    public void onTabChanged(String tag) {
+        int pos = this.mTabHost.getCurrentTab();
+        this.mViewPager.setCurrentItem(pos);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int arg0) {
+    }
+
+    // Manages the Page changes, synchronizing it with Tabs
+    @Override
+    public void onPageScrolled(int arg0, float arg1, int arg2) {
+        int pos = this.mViewPager.getCurrentItem();
+        this.mTabHost.setCurrentTab(pos);
+    }
+
+    @Override
+    public void onPageSelected(int arg0) {
+    }
+
+    private List<Fragment> getFragments(){
+        List<Fragment> fList = new ArrayList<Fragment>();
+
+        AllGuidesTab f1 = new AllGuidesTab();
+        MyGuidesTab f2 = new MyGuidesTab();
+        NearByGuidesTab f3 = new NearByGuidesTab();
+        fList.add(f1);
+        fList.add(f2);
+        fList.add(f3);
+
+        return fList;
+    }
+
+    // Tabs Creation
+    private void initialiseTabHost() {
+        mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup();
+
+        GuidesActivity.AddTab(this, this.mTabHost, this.mTabHost.newTabSpec("all_guides").setIndicator(getString(R.string.all_guides)));
+        GuidesActivity.AddTab(this, this.mTabHost, this.mTabHost.newTabSpec("my_guides").setIndicator(getString(R.string.my_guides)));
+        GuidesActivity.AddTab(this, this.mTabHost, this.mTabHost.newTabSpec("nearby_guides").setIndicator(getString(R.string.nearby_guides)));
+
+        mTabHost.setOnTabChangedListener(this);
+    }
+   
 }
---
+++ src/org/inaturalist/android/MyPageAdapter.java
@@ -0,0 +1,25 @@
+package org.inaturalist.android;
+
+import java.util.List;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MyPageAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragments;
+
+    public MyPageAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return this.fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
+}
