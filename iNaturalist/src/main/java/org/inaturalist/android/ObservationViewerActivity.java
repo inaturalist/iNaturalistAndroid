5b5a155a84f7e8d3ae8e50d8dd39c4e1834015c8
budowski <budowski@gmail.com>
2016-04-05 17:58:59 +0900

    Fix crash (close #153)

--- iNaturalist/src/main/java/org/inaturalist/android/ObservationViewerActivity.java
+++ iNaturalist/src/main/java/org/inaturalist/android/ObservationViewerActivity.java
@@ -1555,7 +1555,6 @@ public class ObservationViewerActivity extends SherlockFragmentActivity {
             mFavorites = favResults;
             mProjects = projectResults;
 
-            mObservation = observation;
             loadObservationIntoUI();
             setupMap();
             refreshActivity();
