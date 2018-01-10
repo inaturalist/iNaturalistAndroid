ee5e80732c347434bb3fe2ac76117a4198fef5b0
Yaron Budowski <budowski@gmail.com>
2018-01-10 16:14:33 +0200

    Mentions auto complete - fix overlapping in some devices

--- iNaturalist/src/main/java/org/inaturalist/android/MentionsAutoComplete.java
+++ iNaturalist/src/main/java/org/inaturalist/android/MentionsAutoComplete.java
@@ -5,9 +5,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
@@ -23,6 +25,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -302,11 +305,6 @@ public class MentionsAutoComplete implements TextWatcher, AdapterView.OnItemClic
         int width = displayMetrics.widthPixels;
         mPopupWindow.setWidth(width - (int)mHelper.dpToPx(30));
 
-        WindowManager windowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
-        Display display = windowManager.getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-
         int pos = mEditText.getSelectionStart();
         Layout layout = mEditText.getLayout();
         int line = layout.getLineForOffset(pos);
@@ -314,7 +312,11 @@ public class MentionsAutoComplete implements TextWatcher, AdapterView.OnItemClic
         int ascent = layout.getLineAscent(line);
         float y = baseline + ascent;
 
-        mPopupWindow.setHeight((int)(size.x - mEditText.getHeight() + y - mEditText.getScrollY() - mHelper.dpToPx(70)));
+        Rect r = new Rect();
+        mEditText.getWindowVisibleDisplayFrame(r);
+
+        mPopupWindow.setHeight((int)(r.bottom - mEditText.getHeight() + y - mEditText.getScrollY() - mHelper.dpToPx(70)));
+
 
         mPopupWindow.setClippingEnabled(false);
 
