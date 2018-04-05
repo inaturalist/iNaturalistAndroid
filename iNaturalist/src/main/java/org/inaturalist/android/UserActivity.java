799b1061e5b92fdb4eae0a5e3b42445895cee6da
Yaron Budowski <budowski@gmail.com>
2018-04-05 19:37:43 +0200

    Fix to UserActivity crash

--- iNaturalist/src/main/java/org/inaturalist/android/UserActivity.java
+++ iNaturalist/src/main/java/org/inaturalist/android/UserActivity.java
@@ -569,6 +569,8 @@ public class UserActivity extends BaseFragmentActivity implements UserActivities
     @Override
     public void onUpdateViewed(Observation obs, int position) {
         try {
+            if (mActivities == null) return;
+
             JSONObject item = obs.id ==  mActivities.get(position).getInt("resource_id") ? mActivities.get(position) : mFollowingActivities.get(position) ;
             if (!item.getBoolean("viewed")) {
                 SharedPreferences prefs = getSharedPreferences("iNaturalistPreferences", MODE_PRIVATE);
