90c49925e72d07e9bde1d2f8801f63809174f183
Yaron Budowski <budowski@gmail.com>
2019-07-05 17:42:32 +0800

    When registering via app - set user timezone according to current coordinates

--- iNaturalist/src/main/java/org/inaturalist/android/ObservationListActivity.java
+++ iNaturalist/src/main/java/org/inaturalist/android/ObservationListActivity.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TimeZone;
 
 import org.apache.commons.lang3.StringUtils;
 import org.inaturalist.android.INaturalistApp.INotificationCallback;
