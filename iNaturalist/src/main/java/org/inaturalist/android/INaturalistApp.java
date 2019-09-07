eba3bf0cfac28426b63220737d545cc349229238
Yaron Budowski <budowski@gmail.com>
2019-09-07 13:08:01 -0700

    #694 - use English locale when sending timestamp observed on string for observation

--- iNaturalist/src/main/java/org/inaturalist/android/INaturalistApp.java
+++ iNaturalist/src/main/java/org/inaturalist/android/INaturalistApp.java
@@ -103,7 +103,7 @@ public class INaturalistApp extends MultiDexApplication {
 	private boolean mIsSyncing = false;
     public static Integer VERSION = 1;
     public static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-    public static SimpleDateFormat DATETIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd h:mm:ss a z");
+    public static SimpleDateFormat DATETIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd h:mm:ss a z", Locale.ENGLISH);
     public static SimpleDateFormat SHORT_DATE_FORMAT = new SimpleDateFormat("d MMM yyyy");
     public static SimpleDateFormat SHORT_TIME_FORMAT = new SimpleDateFormat("hh:mm a z");
     private static Integer SYNC_NOTIFICATION = 3;
