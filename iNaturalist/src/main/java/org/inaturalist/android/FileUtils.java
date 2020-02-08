fec18e4f01d09900f04ddfe7c17bc0a91aae32a1
Yaron Budowski <budowski@gmail.com>
2020-02-08 17:51:49 +0200

    #770 - edit photos from obs viewer

--- iNaturalist/src/main/java/org/inaturalist/android/FileUtils.java
+++ iNaturalist/src/main/java/org/inaturalist/android/FileUtils.java
@@ -37,6 +37,11 @@ import org.tinylog.Logger;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.DecimalFormat;
 import java.util.Comparator;
 
@@ -525,4 +530,51 @@ public class FileUtils {
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         return intent;
     }
+
+    public static void copyFile(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        copyInputStream(in, dst);
+    }
+
+    public static void copyFileFromUri(Context context, Uri uri, File dst) throws IOException {
+        InputStream in = context.getContentResolver().openInputStream(uri);
+        copyInputStream(in, dst);
+    }
+
+    public static void copyInputStream(InputStream in, File dst) throws IOException {
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+            } finally {
+                out.close();
+            }
+        } finally {
+            in.close();
+        }
+    }
+
+    public static String getExtension(Context context, Uri uri) {
+        String extension;
+
+        //Check uri format to avoid null
+        if (uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+            //If scheme is a content
+            final MimeTypeMap mime = MimeTypeMap.getSingleton();
+            extension = mime.getExtensionFromMimeType(context.getContentResolver().getType(uri));
+        } else {
+            //If scheme is a File
+            //This will replace white spaces with %20 and also other special characters. This will avoid returning null values on file name with spaces and special characters.
+            extension = MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(new File(uri.getPath())).toString());
+
+        }
+
+        return extension;
+    }
+
 }
