66649c173e5021867f0dc0736f50ac506af20be2
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-09-08 18:24:02 -0700

    Change new_activies string to a plural

--- iNaturalist/src/main/java/org/inaturalist/android/BaseFragmentActivity.java
+++ iNaturalist/src/main/java/org/inaturalist/android/BaseFragmentActivity.java
@@ -935,7 +935,11 @@ public class BaseFragmentActivity extends AppCompatActivity {
         int unreadActivities = prefs.getInt("unread_activities", 0);
         TextView activityBadge = (TextView)findViewById(R.id.activity_badge);
         activityBadge.setVisibility(unreadActivities > 0 ? View.VISIBLE : View.GONE);
-        activityBadge.setText(String.format(getString(R.string.new_activities), unreadActivities));
+        activityBadge.setText(getResources().getQuantityString(
+                R.plurals.new_activities,
+                unreadActivities,
+                unreadActivities
+        ));
     }
 
     private void refreshUnreadMessages() {
