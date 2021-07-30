bd9e99e7daa6772dfb6f87352237d1513da28d21
ejberry <ellisjamesberry@gmail.com>
2021-07-30 18:41:30 -0500

    Finished up license tests

--- iNaturalist/src/androidTest/java/org/inaturalist/android/AboutLicensesActivityTest.kt
+++ iNaturalist/src/androidTest/java/org/inaturalist/android/AboutLicensesActivityTest.kt
@@ -58,6 +58,139 @@ class AboutLicensesActivityTest {
             )
     }
 
+    @Test
+    fun verifyAttributionNonCommercialLicensesSection() {
+        onView(withId(R.id.licenses_list))
+            .perform(
+                RecyclerViewActions.actionOnItem<AboutLicensesAdapter.ViewHolder>(
+                    hasDescendant(withText(ATTRIBUTION_NONCOMMERCIAL)),
+                    RecyclerViewHelpers.requireAssertions(
+                        listOf(
+                            {
+                                it.findViewById<View>(R.id.license_link).visibility == View.VISIBLE
+                            },
+                            {
+                                it.findViewById<View>(R.id.wikimedia).visibility == View.GONE
+                            },
+                            {
+                                it.findViewById<View>(R.id.gbif).visibility == View.VISIBLE
+                            }
+                        )
+                    )
+                )
+            )
+    }
+    @Test
+    fun verifyAttributionNonCommercialShareAlikeLicensesSection() {
+        onView(withId(R.id.licenses_list))
+            .perform(
+                RecyclerViewActions.actionOnItem<AboutLicensesAdapter.ViewHolder>(
+                    hasDescendant(withText(ATTRIBUTION_NONCOMERCIAL_SHAREALIKE)),
+                    RecyclerViewHelpers.requireAssertions(
+                        listOf(
+                            {
+                                it.findViewById<View>(R.id.license_link).visibility == View.VISIBLE
+                            },
+                            {
+                                it.findViewById<View>(R.id.wikimedia).visibility == View.GONE
+                            },
+                            {
+                                it.findViewById<View>(R.id.gbif).visibility == View.GONE
+                            }
+                        )
+                    )
+                )
+            )
+    }
+    @Test
+    fun verifyAttributionNonCommercialNoDerivsLicensesSection() {
+        onView(withId(R.id.licenses_list))
+            .perform(
+                RecyclerViewActions.actionOnItem<AboutLicensesAdapter.ViewHolder>(
+                    hasDescendant(withText(ATTRIBUTION_NONCOMERCIAL_NODERIVS)),
+                    RecyclerViewHelpers.requireAssertions(
+                        listOf(
+                            {
+                                it.findViewById<View>(R.id.license_link).visibility == View.VISIBLE
+                            },
+                            {
+                                it.findViewById<View>(R.id.wikimedia).visibility == View.GONE
+                            },
+                            {
+                                it.findViewById<View>(R.id.gbif).visibility == View.GONE
+                            }
+                        )
+                    )
+                )
+            )
+    }
+    @Test
+    fun verifyAttributionNoDerivsLicensesSection() {
+        onView(withId(R.id.licenses_list))
+            .perform(
+                RecyclerViewActions.actionOnItem<AboutLicensesAdapter.ViewHolder>(
+                    hasDescendant(withText(ATTRIBUTION_NODERIVS)),
+                    RecyclerViewHelpers.requireAssertions(
+                        listOf(
+                            {
+                                it.findViewById<View>(R.id.license_link).visibility == View.VISIBLE
+                            },
+                            {
+                                it.findViewById<View>(R.id.wikimedia).visibility == View.GONE
+                            },
+                            {
+                                it.findViewById<View>(R.id.gbif).visibility == View.GONE
+                            }
+                        )
+                    )
+                )
+            )
+    }
+    @Test
+    fun verifyAttributionShareAlikeSection() {
+        onView(withId(R.id.licenses_list))
+            .perform(
+                RecyclerViewActions.actionOnItem<AboutLicensesAdapter.ViewHolder>(
+                    hasDescendant(withText(ATTRIBUTION_SHAREALIKE)),
+                    RecyclerViewHelpers.requireAssertions(
+                        listOf(
+                            {
+                                it.findViewById<View>(R.id.license_link).visibility == View.VISIBLE
+                            },
+                            {
+                                it.findViewById<View>(R.id.wikimedia).visibility == View.VISIBLE
+                            },
+                            {
+                                it.findViewById<View>(R.id.gbif).visibility == View.GONE
+                            }
+                        )
+                    )
+                )
+            )
+    }
+    @Test
+    fun verifyAllRightsReservedSection() {
+        onView(withId(R.id.licenses_list))
+            .perform(
+                RecyclerViewActions.actionOnItem<AboutLicensesAdapter.ViewHolder>(
+                    hasDescendant(withText(ALL_RIGHTS_RESERVED)),
+                    RecyclerViewHelpers.requireAssertions(
+                        listOf(
+                            {
+                                it.findViewById<View>(R.id.license_link).visibility == View.GONE
+                            },
+                            {
+                                it.findViewById<View>(R.id.wikimedia).visibility == View.GONE
+                            },
+                            {
+                                it.findViewById<View>(R.id.gbif).visibility == View.GONE
+                            }
+                        )
+                    )
+                )
+            )
+    }
+
     companion object {
         private const val PUBLIC_DOMAIN_LICENSE = "Public Domain"
         private const val ATTRIBUTION_LICENSE = "Attribution"
