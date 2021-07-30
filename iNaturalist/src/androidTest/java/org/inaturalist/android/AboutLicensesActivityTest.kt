359cce43b0cb68ec878434749e4235979d086385
ejberry <ellisjamesberry@gmail.com>
2021-07-29 22:10:15 -0500

    Stub license test

---
+++ iNaturalist/src/androidTest/java/org/inaturalist/android/AboutLicensesActivityTest.kt
@@ -0,0 +1,17 @@
+package org.inaturalist.android
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+
+class AboutLicensesActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(AboutLicensesActivity::class.java)
+
+    @Test
+    fun verifyAboutLicensesSections() {
+
+
+    }
+}
