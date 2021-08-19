9dc0d0b8ef13b64fe3099848e6370c01c158ce02
Yaron Budowski <budowski@gmail.com>
2021-08-19 15:18:17 +0200

    #1101 - Use nearest neighbor resampling when submitting images to CV

--- iNaturalist/src/main/java/org/inaturalist/android/ImageUtils.java
+++ iNaturalist/src/main/java/org/inaturalist/android/ImageUtils.java
@@ -436,7 +436,7 @@ public class ImageUtils {
                 } else {
                     // The Smooth rescale library has issues with Older Android versions (causes crashes) - use
                     // built-in Android resizing
-                    resizedBitmap = Bitmap.createScaledBitmap(resizedBitmap, newWidth, newHeight, true);
+                    resizedBitmap = Bitmap.createScaledBitmap(resizedBitmap, newWidth, newHeight, !noLanczos);
                 }
             }
 
