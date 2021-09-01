cbd38616b5f88475672468bdcfa62cb337e81310
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2021-08-31 19:09:21 -0700

    Tried to restore smoothrescale test, but exclude it from gradlew command

--- smoothrescale/src/androidTest/java/com/schokoladenbrown/smoothrescale/ApplicationTest.java
+++ smoothrescale/src/androidTest/java/com/schokoladenbrown/smoothrescale/ApplicationTest.java
@@ -1,13 +1,13 @@
 package com.schokoladenbrown.smoothrescale;
 
-// import android.app.Application;
-// import android.test.ApplicationTestCase;
+import android.app.Application;
+import android.test.ApplicationTestCase;
 
-// /**
-//  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
-//  */
-// public class ApplicationTest extends ApplicationTestCase<Application> {
-//     public ApplicationTest() {
-//         super(Application.class);
-//     }
-// }
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
