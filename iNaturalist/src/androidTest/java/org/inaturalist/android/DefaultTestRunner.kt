83bff6468bc41ed46a1779a0ae39c6a6fa7786c5
Ellis Berry <BerryE1@aetna.com>
2021-09-02 12:09:03 -0600

    Espresso first-time-use tutorial test failure (#1114)
    Commit default value into shared prefs for tests, so the tutorial doesn't present unexpected views.
    
    Co-authored-by: Ellis Berry <ellisjamesberry@gmail.com>

--- iNaturalist/src/androidTest/java/org/inaturalist/android/DefaultTestRunner.kt
+++ iNaturalist/src/androidTest/java/org/inaturalist/android/DefaultTestRunner.kt
@@ -1,12 +1,15 @@
 package org.inaturalist.android
 
+import android.content.Context
+import android.os.Bundle
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.accessibility.AccessibilityChecks
 import androidx.test.espresso.base.DefaultFailureHandler
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnitRunner
 import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResult
 
-class DefaultTestRunner: AndroidJUnitRunner() {
+class DefaultTestRunner : AndroidJUnitRunner() {
 
     override fun onStart() {
         AccessibilityChecks
@@ -20,6 +23,19 @@ class DefaultTestRunner: AndroidJUnitRunner() {
                     .handle(error, viewMatcher)
             }.getOrThrow()
         }
+
+        disableFirstTimeUserTutorial()
+
         super.onStart()
+    }
+
+    private fun disableFirstTimeUserTutorial() {
+        val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val preferencesEditor = targetContext.getSharedPreferences(
+            "iNaturalistPreferences",
+            Context.MODE_PRIVATE
+        ).edit()
+        preferencesEditor.putBoolean("first_time", false)
+        preferencesEditor.commit()
     }
 }
