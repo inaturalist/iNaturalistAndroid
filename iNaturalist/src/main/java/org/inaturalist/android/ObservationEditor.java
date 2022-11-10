4d7855b6687f002e22ed38e25f6eb85b75b32f2a
Yaron Budowski <budowski@gmail.com>
2022-11-10 13:14:04 +0100

    #1232 - crash fix

--- iNaturalist/src/main/java/org/inaturalist/android/ObservationEditor.java
+++ iNaturalist/src/main/java/org/inaturalist/android/ObservationEditor.java
@@ -2813,9 +2813,16 @@ public class ObservationEditor extends Fragment {
                                 "(_observation_id=?) and ((is_deleted = 0) OR (is_deleted IS NULL)) and (position = ?)",
                                 new String[]{mObservation._id.toString(), String.valueOf(index)},
                                 ObservationPhoto.DEFAULT_SORT_ORDER);
-                        ObservationPhoto op = new ObservationPhoto(c);
-                        c.close();
-                        mPhotosRemoved.add(op);
+                        ObservationPhoto op;
+                        if (c.getCount() > 0) {
+                            op = new ObservationPhoto(c);
+                            mPhotosRemoved.add(op);
+                            c.close();
+                        } else {
+                            Logger.tag(TAG).error("Failed to find old photo: " + index);
+                            c.close();
+                            continue;
+                        }
 
                         // Mark photo as deleted
                         ContentValues cv = new ContentValues();
